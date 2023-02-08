38ff7f26938f6b441e69f17b4cac4d5ed345f8e9
Jeremy641 <jeremy641@gmail.com>
2023-02-09 00:57:43 +0800

    畫布噴漆效果製作與互動製作

--- Unity_VRGame_throwball/Assets/Scripts/BallManager.cs
+++ Unity_VRGame_throwball/Assets/Scripts/BallManager.cs
@@ -8,4 +8,25 @@ public class BallManager : MonoBehaviour
     public GameObject effectObject;
     [Header("碰撞音效")]
     public AudioClip soundHit;
+    [Header("圖片的角度")]
+    public Vector3 pictureAngle;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //print(collision.gameObject.name);
+
+        //如果 碰到物件的名稱 等於 畫布
+        //就 生成 圖片和特效
+        if (collision.gameObject.name == "畫布")
+        {
+            Instantiate(effectObject, transform.position, transform.rotation);
+
+            // Quaternion.Euler(角度)
+            Instantiate(pictureObject, transform.position, Quaternion.Euler(pictureAngle));
+
+            //刪除(此物件)
+            Destroy(gameObject);
+        }
+
+    }
 }
